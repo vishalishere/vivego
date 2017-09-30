6c52de9b581fa5de6b12058e5e3276c087172dcd
Esben Carlsen <esben@vergic.com>
2017-09-30 22:54:00 +0200

    Added WampSharp distributed backplane

---
+++ src/vivego.WampSharp.Proto.SubPub.Backplane.Playground/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+using vivego.core;
+
+using WampSharp.V2;
+using WampSharp.V2.Realm;
+
+namespace vivego.WampSharp.Proto.SubPub.Backplane.Playground
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			int serverPort = PortUtils.FindAvailablePortIncrementally(18889);
+			string serverAddress = "ws://127.0.0.1:" + serverPort + "/ws";
+			WampHost host = new DefaultWampHost(serverAddress);
+			IWampHostedRealm realm = host.RealmContainer.GetRealmByName("vivego");
+			realm.EnableDistributedBackplane();
+			host.Open();
+
+			DefaultWampChannelFactory channelFactory = new DefaultWampChannelFactory();
+			IWampChannel wampChannel = channelFactory.CreateJsonChannel(serverAddress, "vivego");
+			wampChannel.Open().Wait();
+			ISubject<string> subject = wampChannel.RealmProxy.Services.GetSubject<string>("com.vivego");
+			subject.Subscribe(s => { Console.Out.WriteLine("Client Received " + s); });
+
+			while (true)
+			{
+				var input = Console.ReadLine();
+				subject.OnNext(input);
+			}
+		}
+	}
+}
---
+++ src/vivego.WampSharp.Proto.SubPub.Backplane/RealmExtensions.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Reactive.Subjects;
+
+using Microsoft.Extensions.Logging;
+
+using vivego.core;
+using vivego.Discovery;
+using vivego.Discovery.DotNetty;
+using vivego.Proto.ClusterProvider;
+using vivego.Proto.PubSub;
+using vivego.Serializer.Abstractions;
+using vivego.Serializer.MessagePack;
+
+using WampSharp.V2.Realm;
+
+namespace vivego.WampSharp.Proto.SubPub.Backplane
+{
+	public static class RealmExtensions
+	{
+		private static IEnumerable<IPAddress> GetMyIpAddress()
+		{
+			foreach (NetworkInterface allNetworkInterface in NetworkInterface.GetAllNetworkInterfaces())
+			{
+				if (allNetworkInterface.OperationalStatus != OperationalStatus.Up)
+				{
+					continue;
+				}
+
+				IPInterfaceProperties ipProperties = allNetworkInterface.GetIPProperties();
+				if (ipProperties.GatewayAddresses.Count == 0)
+				{
+					continue;
+				}
+
+				foreach (UnicastIPAddressInformation ip in allNetworkInterface.GetIPProperties().UnicastAddresses)
+				{
+					if (ip.Address.Equals(IPAddress.Loopback))
+					{
+						continue;
+					}
+
+					if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+					{
+						yield return ip.Address;
+					}
+				}
+			}
+		}
+
+		private static IPEndPoint[] QueryEndPointsUdp(string clusterId, IPAddress ipAddress, int serverPort)
+		{
+			IPEndPoint protoActorServerEndpoint = new IPEndPoint(ipAddress, serverPort);
+			new UdpBroadcastServer(protoActorServerEndpoint, clusterId).Run();
+			UdpDiscoverClientFactory clientFactory =
+				new UdpDiscoverClientFactory(clusterId, replyWaitTimeout: TimeSpan.FromSeconds(2));
+			IPEndPoint[] endPoints = clientFactory.DiscoverEndPoints();
+			return endPoints;
+		}
+
+		private static IPEndPoint[] QueryEndPointsMulticast(string clusterId, IPAddress ipAddress, int serverPort)
+		{
+			IPEndPoint protoActorServerEndpoint = new IPEndPoint(ipAddress, serverPort);
+			new DiscoverMulticastServer(protoActorServerEndpoint, clusterId).Run();
+			MulticastDiscoverClientFactory clientFactory =
+				new MulticastDiscoverClientFactory(clusterId, replyWaitTimeout: TimeSpan.FromSeconds(2));
+			IPEndPoint[] endPoints = clientFactory.DiscoverEndPoints();
+			return endPoints;
+		}
+
+		public static IDisposable EnableDistributedBackplane(this IWampHostedRealm realm)
+		{
+			IPAddress ipAddress = GetMyIpAddress().First();
+			int serverPort = PortUtils.FindAvailablePort();
+			IPEndPoint[] endPoints = QueryEndPointsMulticast(realm.Name, ipAddress, serverPort);
+			ILoggerFactory loggerFactory = new LoggerFactory().AddConsole(LogLevel.Debug);
+			loggerFactory
+				.CreateLogger("Auto")
+				.LogDebug("Discovered endpoints: {0}", string.Join(";", endPoints.Select(endPoint => endPoint.ToString())));
+			ISerializer<byte[]> serializer = new MessagePackSerializer();
+			IPublishSubscribe pubSub = PublishSubscribe.StartCluster(realm.Name,
+				ipAddress.ToString(),
+				serverPort,
+				new StaticClusterProvider(TimeSpan.FromSeconds(1), endPoints),
+				serializer,
+				loggerFactory
+			);
+
+			return EnableDistributedBackplane(realm, pubSub);
+		}
+
+		public static IDisposable EnableDistributedBackplane(this IWampHostedRealm realm,
+			IPublishSubscribe publishSubscribe)
+		{
+			ConcurrentBag<IDisposable> disposables = new ConcurrentBag<IDisposable>();
+			realm.TopicContainer.TopicCreated += (sender, args) =>
+			{
+				ISubject<object> topicSubject = realm.Services.GetSubject<object>(args.Topic.TopicUri);
+				IDisposable wampSubscription = topicSubject.Subscribe(o => publishSubscribe.Publish(args.Topic.TopicUri, o));
+				IDisposable pubSubSubscription = publishSubscribe
+					.Observe<object>(args.Topic.TopicUri)
+					.Subscribe(topicSubject);
+				disposables.Add(wampSubscription);
+				disposables.Add(pubSubSubscription);
+			};
+
+			return new AnonymousDisposable(() =>
+			{
+				while (disposables.TryTake(out IDisposable disposable))
+				{
+					disposable.Dispose();
+				}
+			});
+		}
+	}
+}
