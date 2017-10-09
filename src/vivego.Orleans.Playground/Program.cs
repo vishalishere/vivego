078cb420ccbb4eab3a6cc417786a674fce11a7c0
Esben Carlsen <esben@vergic.com>
2017-10-10 00:30:14 +0200

    Added Orleans Playground. Not working becuase of dependency conflict

---
+++ src/vivego.Orleans.Playground/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+using Orleans.Runtime.Configuration;
+using Orleans.Runtime.Host;
+
+using vivego.Orleans.Providers;
+using vivego.Proto.PubSub;
+using vivego.Serializer.Abstractions;
+
+namespace vivego.Orleans.Playground
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			ClusterConfiguration siloConfig = ClusterConfiguration
+				.LocalhostPrimarySilo()
+				.AddPublishSubscribeStreamProvider();
+			siloConfig.UseStartupType<OrleansStartup>();
+			using (SiloHost silo = new SiloHost("Test Silo", siloConfig))
+			{
+				silo.InitializeOrleansSilo();
+				silo.StartOrleansSilo();
+
+				Console.WriteLine("Press Enter to close.");
+
+				// wait here
+				Console.ReadLine();
+
+				// shut the silo down after we are done.
+				silo.ShutdownOrleansSilo();
+			}
+		}
+	}
+
+	public class OrleansStartup
+	{
+		public IServiceProvider ConfigureServices(IServiceCollection services)
+		{
+			services.AddSingleton<ILoggerFactory, LoggerFactory>();
+			services.AddSingleton<ISerializer<byte[]>, MessagePackSerializer>();
+			services.AddSingleton<IPublishSubscribe, PublishSubscribe>();
+			return new DefaultServiceProviderFactory()
+				.CreateServiceProvider(services);
+		}
+	}
+}
---
+++ src/vivego.Orleans.Providers/ClusterConfigurationExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Orleans.Runtime.Configuration;
+
+using vivego.Orleans.Providers.Stream;
+
+namespace vivego.Orleans.Providers
+{
+	public static class ClusterConfigurationExtensions
+	{
+		public static ClusterConfiguration AddPublishSubscribeStreamProvider(this ClusterConfiguration clusterConfiguration,
+			string providerName = "SMSProvider")
+		{
+			if (clusterConfiguration == null)
+			{
+				throw new ArgumentNullException(nameof(clusterConfiguration));
+			}
+
+			if (string.IsNullOrWhiteSpace(providerName))
+			{
+				throw new ArgumentNullException(nameof(providerName));
+			}
+
+			clusterConfiguration.Globals.RegisterStreamProvider<PublishSubscribeStreamProvider>(providerName);
+
+			return clusterConfiguration;
+		}
+	}
+}
