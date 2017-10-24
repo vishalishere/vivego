2ba009e9dfe4a01aa1dcd81be465a165324c02f2
Esben Carlsen <esben@vergic.com>
2017-10-24 22:42:09 +0200

    added vivego.Serializer.NewtonSoft

---
+++ src/vivego.Serializer.NewtonSoft/NewtonSoftJsonBinarySerializer.cs
@@ -0,0 +1,72 @@
+﻿using System.Globalization;
+using System.IO;
+using System.Threading;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
+
+using vivego.Serializer.Abstractions;
+
+namespace vivego.Serializer.NewtonSoft
+{
+	public class NewtonSoftJsonBinarySerializer : ISerializer<byte[]>
+	{
+		private readonly ThreadLocal<JsonSerializer> _jsonSerializer;
+
+		public NewtonSoftJsonBinarySerializer() : this(GetDefaultJsonSerializerSettings())
+		{
+		}
+
+		public NewtonSoftJsonBinarySerializer(JsonSerializerSettings jsonSerializerSettings)
+		{
+			_jsonSerializer = new ThreadLocal<JsonSerializer>(() => JsonSerializer.Create(jsonSerializerSettings));
+		}
+
+		public TType Deserialize<TType>(byte[] source)
+		{
+			if (source == null
+				|| source.Length == 0)
+			{
+				return default;
+			}
+
+			using (MemoryStream memoryStream = new MemoryStream(source))
+			using (BsonDataReader bsonDataReader = new BsonDataReader(memoryStream))
+			{
+				return _jsonSerializer.Value.Deserialize<TType>(bsonDataReader);
+			}
+		}
+
+		public byte[] Serialize<TType>(TType type)
+		{
+			if (type == null)
+			{
+				return null;
+			}
+
+			using (MemoryStream memoryStream = new MemoryStream())
+			using (BsonDataWriter bsonDataWriter = new BsonDataWriter(memoryStream))
+			{
+				_jsonSerializer.Value.Serialize(bsonDataWriter, type, typeof(TType));
+				return memoryStream.ToArray();
+			}
+		}
+
+		public static JsonSerializerSettings GetDefaultJsonSerializerSettings()
+		{
+			return new JsonSerializerSettings
+			{
+				TypeNameHandling = TypeNameHandling.All,
+				PreserveReferencesHandling = PreserveReferencesHandling.Objects,
+				NullValueHandling = NullValueHandling.Ignore,
+				DateTimeZoneHandling = DateTimeZoneHandling.Utc,
+				DateFormatHandling = DateFormatHandling.IsoDateFormat,
+				Culture = CultureInfo.InvariantCulture,
+				MissingMemberHandling = MissingMemberHandling.Ignore,
+				DateParseHandling = DateParseHandling.DateTime,
+				ConstructorHandling = ConstructorHandling.AllowNonPublicDefaultConstructor,
+				TypeNameAssemblyFormatHandling = TypeNameAssemblyFormatHandling.Simple
+			};
+		}
+	}
+}
---
+++ src/vivego.Serializer.NewtonSoft/NewtonSoftJsonSerializer.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+using Newtonsoft.Json;
+
+using vivego.Serializer.Abstractions;
+
+namespace vivego.Serializer.NewtonSoft
+{
+	public class NewtonSoftJsonSerializer : ISerializer<string>
+	{
+		private readonly ThreadLocal<JsonSerializer> _jsonSerializer;
+
+		public NewtonSoftJsonSerializer() : this(GetDefaultJsonSerializerSettings())
+		{
+		}
+
+		public NewtonSoftJsonSerializer(JsonSerializerSettings jsonSerializerSettings)
+		{
+			_jsonSerializer = new ThreadLocal<JsonSerializer>(() => JsonSerializer.Create(jsonSerializerSettings));
+		}
+
+		public TType Deserialize<TType>(string source)
+		{
+			if (!string.IsNullOrEmpty(source))
+			{
+				return (TType) Deserialize(source, typeof(TType));
+			}
+
+			return default;
+		}
+
+		public string Serialize<TType>(TType type)
+		{
+			if (type == null)
+			{
+				return null;
+			}
+
+			StringWriter stringWriter = new StringWriter(new StringBuilder(256), CultureInfo.InvariantCulture);
+			using (JsonTextWriter jsonTextWriter = new JsonTextWriter(stringWriter))
+			{
+				_jsonSerializer.Value.Serialize(jsonTextWriter, type, typeof(TType));
+			}
+
+			return stringWriter.ToString();
+		}
+
+		public object Deserialize(string source, Type type = null)
+		{
+			if (string.IsNullOrEmpty(source))
+			{
+				return null;
+			}
+
+			using (JsonTextReader jsonTextReader = new JsonTextReader(new StringReader(source)))
+			{
+				return type == null
+					? _jsonSerializer.Value.Deserialize(jsonTextReader)
+					: _jsonSerializer.Value.Deserialize(jsonTextReader, type);
+			}
+		}
+
+		public static JsonSerializerSettings GetDefaultJsonSerializerSettings()
+		{
+			return new JsonSerializerSettings
+			{
+				TypeNameHandling = TypeNameHandling.All,
+				PreserveReferencesHandling = PreserveReferencesHandling.Objects,
+				NullValueHandling = NullValueHandling.Ignore,
+				DateTimeZoneHandling = DateTimeZoneHandling.Utc,
+				DateFormatHandling = DateFormatHandling.IsoDateFormat,
+				Culture = CultureInfo.InvariantCulture,
+				MissingMemberHandling = MissingMemberHandling.Ignore,
+				DateParseHandling = DateParseHandling.DateTime,
+				ConstructorHandling = ConstructorHandling.AllowNonPublicDefaultConstructor,
+				TypeNameAssemblyFormatHandling = TypeNameAssemblyFormatHandling.Simple
+			};
+		}
+	}
+}
